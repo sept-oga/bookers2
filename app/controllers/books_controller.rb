19fff9c2200576d8f7dd3763e345211244df7023
sept-oga <herbst.sechs@gmail.com>
2024-10-17 12:55:55 +0000

    [Break] 4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,6 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @book = Book.new
     @user = current_user
   end
 
@@ -22,8 +21,14 @@ class BooksController < ApplicationController
   end
   
   def edit
+    @book = Book.find(params[:id])
   end
   
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
   private
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about', to: 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
