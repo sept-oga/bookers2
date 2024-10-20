23c24bc1dce886bbd795aa8650bb84d94e11c18c
sept-oga <herbst.sechs@gmail.com>
2024-10-20 06:18:20 +0000

    [Finish] 4-25

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about', to: 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :index, :update]
   
 end
